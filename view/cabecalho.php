d489c4b73da4469703035cbbe00e36d2cc631e0e
NicolasBabolin <nicolas.santos150@etecia.com.br>
2020-07-14 19:14:22 -0300

    projeto até o passo #9

--- view/cabecalho.php
+++ view/cabecalho.php
@@ -10,6 +10,6 @@
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style.css">
     <title>Clorocine</title>
 </head>
